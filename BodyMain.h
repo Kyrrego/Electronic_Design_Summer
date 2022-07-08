0e94a13abbc497b7e578026122d6b3a2ec4802e4
Zhang Yunrui <ZhangYR_linda@163.com>
2022-07-08 16:53:15 +0800

    add BodyMain.h

--- BodyMain.h
+++ BodyMain.h
@@ -65,4 +65,45 @@ public:
 	/// <param name="joint0">one joint of the bone to draw</param>
 	/// <param name="joint1">other joint of the bone to draw</param>
 	void                    DrawBone(const Joint* pJoints, const D2D1_POINT_2F* pJointPoints, JointType joint0, JointType joint1);
+
+	static DWORD  framenumber;			//骨骼帧编号
+	float spinemid_xin;		//重心
+	float spinemid_yin;
+	float spinemid_zin;
+	float spinemid_xout;
+	float spinemid_yout;
+	float spinemid_zout;
+	float spinemid_x;
+	float spinemid_y;
+	float spinemid_z;
+	float spinebase_yin, spinebase_yout, spinebase_y;
+	float rightfoot_yin = 0, rightfoot_yout = 0, rightfoot_y = 0;
+	float rightAnkle_yin = 0, rightAnkle_yout = 0, rightAnkle_y = 0;
+	float leftfoot_yin = 0, leftfoot_yout = 0, leftfoot_y = 0;
+	float base_foot_in = 0, base_foot_out = 0, base_foot = 0;
+	float spinetemp = 0;
+
+private:
+	IKinectSensor*          m_pKinectSensor;//kinect源
+	ICoordinateMapper*      m_pCoordinateMapper;//用于坐标变换
+	IBodyFrameReader*       m_pBodyFrameReader;//用于骨架数据读取
+	IDepthFrameReader*      m_pDepthFrameReader;//用于深度数据读取
+	IBodyIndexFrameReader*  m_pBodyIndexFrameReader;//用于背景二值图读取
+
+	//
+	////画骨架函数
+	//void DrawBone(const Joint* pJoints, const DepthSpacePoint* depthSpacePosition, JointType joint0, JointType joint1);
+	////画手的状态函数
+	//void DrawHandState(const DepthSpacePoint depthSpacePosition, HandState handState);
+	void Detection(Joint joints[]);
+	double  Distance(Joint p1, Joint p2);
+
+	void AngleHandle(Joint joints[]);
+	int ElbowCalc(Joint Vertex, Joint Vertex1, Joint Vertex2);
+	int HandCalc(Joint Vertex1, Joint Vertex2, Joint Vertex3);
+	int ChestCalc(Joint Vertex1, Joint Vertex2, Joint Vertex3);
+	void ShoulderCalc(Joint Vertex1, Joint Vertex2, int* Address);
+	//显示图像的Mat
+	cv::Mat skeletonImg;		//骨骼图
+	cv::Mat depthImg;			//深度图
 };
